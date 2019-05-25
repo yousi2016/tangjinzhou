df8d43763754b67c4483ed6d20f317cfd91a18b1
yousi2016 <dingyousi888@gmail.com>
2019-05-25 20:29:27 +0800

    change file lujing

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    {{msg}}
+    <div>
+      <input type="text" v-model="info">
+      <button @click="handleClick">添加</button>
+    </div>
+    <ul>
+      <!-- 如果报错得添加 :key='item'，默认item没有重复的-->
+      <todo-item v-for="item in list" :key="item" >
+      <!-- v-slot只能放到template里面使用 -->
+      <!-- 使用itemProps接收子组件TodoItem.vue传递的checked -->
+      <!-- 作用域插槽使用的场景 -->
+          <template v-slot:item='itemProps'>
+              <span :style="{fontSize: '20px', color:itemProps.checked ? 'red' : 'green'}">{{item}}</span>
+          </template>
+      </todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+//引入组件TodouItem.vue
+import TodoItem from "./components/TodoItem";
+//注册组件到components下面才可以使用哦
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "hello geektime",
+      info: "",
+      list: []
+    };
+  },
+  methods: {
+    //通过@click定义的方法
+    handleClick() {
+      console.log(1);
+      this.list.push(this.info);
+      this.info = "";
+    }
+  },
+  components: {
+    TodoItem,
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <li class="item">
+        <input type="checkbox" v-model="checked">
+        <!-- 传递给父组件App.vue checked-->
+        <slot name='item' v-bind="{checked}"></slot>
+    </li>
+</template>
+
+<script>
+export default {
+    //给标签自定义属性item
+    props:['item'],
+    data() {
+        return {
+            checked:false
+        }
+    }
+}
+</script>
+/* scoped是让样式都是独立存在不会因为重名的类名相互污染 */
+<style scoped>
+    /*样式也是全局的 */
+    .item{
+        color: green;
+    }
+</style>
